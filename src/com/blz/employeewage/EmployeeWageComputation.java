b48c9d01e662560b2a684d3ffd8b9215ecdf0cf2
VijaykumarBhavanur <vijaykumarbhavanur@gmail.com>
2022-02-05 08:37:25 +0530

    Added: Employee daily wage logic

--- src/com/blz/employeewage/EmployeeWageComputation.java
+++ src/com/blz/employeewage/EmployeeWageComputation.java
@@ -5,13 +5,21 @@ import java.util.Random;
 public class EmployeeWageComputation {
 	public static void main(String[] args) {
 		
+		int empHrs = 0;
+		int wagePerHour = 20; 
 		Random random = new Random();
 		int randomNum = random.nextInt(2);
 		
 		if(randomNum == 1)
+		{
 			System.out.println("Employee present ");
+			empHrs = 8;
+		}
 		else
 			System.out.println("Employee absent");
+		
+		double salary = empHrs * wagePerHour ;
+		System.out.println("Employee wage: "+salary);
 	}
 
 }
